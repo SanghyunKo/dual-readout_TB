1719dc7032ed46400e0601876102c8d184528b2b
Yunjae Lee <yulee@lxplus736.cern.ch>
2022-08-12 15:57:30 +0200

    update set_env

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,4 @@
-export path_to_install=$(realpath $(dirname -- "$0")/build)
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+export path_to_install=$(realpath $(dirname -- "$0")/install)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$path_to_install/lib64 # or lib based on your architecture
 export PYTHONPATH=$PYTHONPATH:$path_to_install/lib64 # or lib based on your architecture
